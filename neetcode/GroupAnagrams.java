3501cc0dc3fb4008baba6f7e223e1a2311e12612
Hoseop.kim <kimho314@naver.com>
2025-07-23 20:10:27 +0300

    [feat]: add GroupAnagrams

---
+++ neetcode/GroupAnagrams.java
@@ -0,0 +1,35 @@
+package neetcode;
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * https://neetcode.io/problems/anagram-groups?list=neetcode150
+ */
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        GroupAnagrams sol = new GroupAnagrams();
+        System.out.println(
+                sol.groupAnagrams(new String[] {"act", "pots", "tops", "cat", "stop", "hat"})); // [["hat"],["act",
+                                                                                                // "cat"],["stop",
+                                                                                                // "pots",
+                                                                                                // "tops"]]
+        System.out.println(sol.groupAnagrams(new String[] {"x"})); // [["x"]]
+        System.out.println(sol.groupAnagrams(new String[] {""})); // [[""]]
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String elem : strs) {
+            char[] arr = elem.toCharArray();
+            Arrays.sort(arr);
+            String s = new String(arr);
+            map.putIfAbsent(s, new ArrayList<>());
+            map.get(s).add(elem);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
