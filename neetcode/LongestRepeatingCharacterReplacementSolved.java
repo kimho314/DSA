c11e6dbd998b1db81215ae65ee185a9c3b2a94dd
Hoseop.Kim <kimho314@gmail.com>
2025-08-05 22:50:03 +0900

    [feat]: add LongestRepeatingCharacterReplacementSolved

---
+++ neetcode/LongestRepeatingCharacterReplacementSolved.java
@@ -0,0 +1,47 @@
+package neetcode;
+
+import java.util.*;
+
+public class LongestRepeatingCharacterReplacementSolved {
+    public static void main(String[] args) {
+        LongestRepeatingCharacterReplacementSolved sol =
+                new LongestRepeatingCharacterReplacementSolved();
+        System.out.println(sol.characterReplacement("XYYX", 2));
+        System.out.println(sol.characterReplacement("AAABABB", 1));
+    }
+
+    public int characterReplacement(String s, int k) {
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            HashMap<Character, Integer> count = new HashMap<>();
+            int maxf = 0;
+            for (int j = i; j < s.length(); j++) {
+                count.put(s.charAt(j), count.getOrDefault(s.charAt(j), 0) + 1);
+                maxf = Math.max(maxf, count.get(s.charAt(j)));
+                if ((j - i + 1) - maxf <= k) {
+                    res = Math.max(res, j - i + 1);
+                }
+            }
+        }
+        return res;
+    }
+
+    public int characterReplacement2(String s, int k) {
+        HashMap<Character, Integer> count = new HashMap<>();
+        int res = 0;
+
+        int l = 0, maxf = 0;
+        for (int r = 0; r < s.length(); r++) {
+            count.put(s.charAt(r), count.getOrDefault(s.charAt(r), 0) + 1);
+            maxf = Math.max(maxf, count.get(s.charAt(r)));
+
+            while ((r - l + 1) - maxf > k) {
+                count.put(s.charAt(l), count.get(s.charAt(l)) - 1);
+                l++;
+            }
+            res = Math.max(res, r - l + 1);
+        }
+
+        return res;
+    }
+}
