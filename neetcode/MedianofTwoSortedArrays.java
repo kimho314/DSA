d1d383e8eafa6059d269d7e3beb2e562fdd78207
Hoseop.Kim <kimho314@gmail.com>
2025-08-28 18:03:55 +0900

    [feat]: modify MedianofTwoSortedArrays

--- neetcode/MedianofTwoSortedArrays.java
+++ neetcode/MedianofTwoSortedArrays.java
@@ -25,4 +25,35 @@ public class MedianofTwoSortedArrays {
             return (double) (res[len / 2 - 1] + res[len / 2]) / 2.0;
         }
     }
+
+    public double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+        int len = nums1.length + nums2.length;
+        int[] res = new int[len];
+        int idx = 0;
+        int l = 0, r = 0;
+        while (l < nums1.length && r < nums2.length) {
+            if (nums1[l] <= nums2[r]) {
+                res[idx] = nums1[l];
+                l++;
+            } else {
+                res[idx] = nums2[r];
+                r++;
+            }
+
+            idx++;
+        }
+
+        while (l < nums1.length) {
+            res[idx++] = nums1[l++];
+        }
+        while (r < nums2.length) {
+            res[idx++] = nums2[r++];
+        }
+
+        if (len % 2 == 1) {
+            return res[len / 2];
+        } else {
+            return (double) (res[len / 2 - 1] + res[len / 2]) / 2.0;
+        }
+    }
 }
