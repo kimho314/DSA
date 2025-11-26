0b91c6fa9426fbd8745672e5dad1ef729d822e77
Hoseop.Kim <kimho314@gmail.com>
2025-11-26 18:35:04 +0200

    [feat]: modify PacificAtlanticWaterFlow

--- neetcode/PacificAtlanticWaterFlow.java
+++ neetcode/PacificAtlanticWaterFlow.java
@@ -23,6 +23,45 @@ public class PacificAtlanticWaterFlow {
         System.out.println();
     }
 
+    public List<List<Integer>> pacificAtlantic2(int[][] heights) {
+        int ROWS = heights.length;
+        int COLS = heights[0].length;
+        boolean[][] pac = new boolean[ROWS][COLS];
+        boolean[][] atl = new boolean[ROWS][COLS];
+
+        for (int c = 0; c < COLS; c++) {
+            dfs2(0, c, pac, heights);
+            dfs2(ROWS - 1, c, atl, heights);
+        }
+        for (int r = 0; r < ROWS; r++) {
+            dfs2(r, 0, pac, heights);
+            dfs2(r, COLS - 1, atl, heights);
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (pac[r][c] && atl[r][c]) {
+                    res.add(Arrays.asList(r, c));
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private void dfs2(int r, int c, boolean[][] ocean, int[][] heights) {
+        ocean[r][c] = true;
+        for (int[] d : dir) {
+            int nr = r + d[0];
+            int nc = c + d[1];
+            if (nr >= 0 && nr < heights.length && nc >= 0 && nc < heights[0].length
+                    && !ocean[nr][nc] && heights[nr][nc] >= heights[r][c]) {
+                dfs(nr, nc, ocean, heights);
+            }
+        }
+    }
+
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         int r = heights.length;
         int c = heights[0].length;
