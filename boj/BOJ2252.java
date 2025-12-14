88d7ba62b68787cfcfb9105c5c2368fd26806b8c
Hoseop.Kim <kimho314@gmail.com>
2025-12-14 16:34:04 +0200

    [feat]: modify BOJ2252

--- boj/BOJ2252.java
+++ boj/BOJ2252.java
@@ -2,48 +2,70 @@ package boj;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class BOJ2252 {
-    static FastReader sc = new FastReader();
-    static StringBuilder sb = new StringBuilder();
-    static int N, M;
-    static int[] INDEG;
-    static ArrayList<Integer>[] ADJ;
+    private static FastReader SC = new FastReader();
+    private static int M, N;
 
     public static void main(String[] args) {
-        N = sc.nextInt();
-        M = sc.nextInt();
-        ADJ = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) {
-            ADJ[i] = new ArrayList<>();
+        N = SC.nextInt();
+        M = SC.nextInt();
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            adj.add(new ArrayList<>());
         }
-        INDEG = new int[N + 1];
         for (int i = 0; i < M; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            ADJ[x].add(y);
-            INDEG[y]++;
+            int n1 = SC.nextInt();
+            int n2 = SC.nextInt();
+            adj.get(n1 - 1).add(n2 - 1);
         }
 
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 1; i <= N; i++) {
-            if (INDEG[i] == 0) {
-                queue.add(i);
+//        for (int i = 0; i < N; i++) {
+//            List<Integer> list = adj.get(i);
+//            StringBuilder sb = new StringBuilder();
+//            for (int elem : list) {
+//                sb.append(elem + 1).append(" ");
+//            }
+//            IO.println((i + 1) + " " + sb);
+//        }
+
+        List<Integer> res = topologicalSort(adj);
+        for (int elem : res) {
+            System.out.print(elem + " ");
+        }
+    }
+
+    private static List<Integer> topologicalSort(List<List<Integer>> adj) {
+        int[] inDegree = new int[N];
+        for (int i = 0; i < N; i++) {
+            for (int elem : adj.get(i)) {
+                inDegree[elem]++;
             }
         }
 
-        while (!queue.isEmpty()) {
-            int x = queue.poll();
-            sb.append(x).append(' ');
-            for (int y : ADJ[x]) {
-                INDEG[y]--;
-                if (INDEG[y] == 0) {
-                    queue.add(y);
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            if (inDegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            res.add(node + 1);
+
+            for (int elem : adj.get(node)) {
+                inDegree[elem]--;
+                if (inDegree[elem] == 0) {
+                    q.add(elem);
                 }
             }
         }
 
-        System.out.println(sb);
+        return res;
     }
 }
