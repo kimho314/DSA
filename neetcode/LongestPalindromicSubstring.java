e501c6d30940b81a7ea6487ea05ef5305893fba5
Hoseop.Kim <kimho314@gmail.com>
2025-12-24 21:01:46 +0200

    [feat]: add LongestPalindromicSubstring

---
+++ neetcode/LongestPalindromicSubstring.java
@@ -0,0 +1,31 @@
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        LongestPalindromicSubstring sol = new LongestPalindromicSubstring();
+        String res = sol.longestPalindrome("ababd");
+        System.out.println(res);
+        res = sol.longestPalindrome("abbc");
+        System.out.println(res);
+    }
+
+    public String longestPalindrome(String s) {
+        int resIdx = 0;
+        int resLen = 0;
+        int n = s.length();
+
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+                    if (resLen < (j - i + 1)) {
+                        resIdx = i;
+                        resLen = j - i + 1;
+                    }
+                }
+            }
+        }
+
+        return s.substring(resIdx, resIdx + resLen);
+    }
+}
