b1e54f31331072f3b975ac62ed8c83832bfa3b94
Hoseop.Kim <kimho314@gmail.com>
2025-12-29 18:21:39 +0200

    [feat]: modify DeocdeWays

--- neetcode/DecodeWays.java
+++ neetcode/DecodeWays.java
@@ -1,5 +1,3 @@
-
-
 import java.util.*;
 import java.io.*;
 
@@ -12,6 +10,25 @@ public class DecodeWays {
         System.out.println(res); // 3
         res = sol.numDecodings("06");
         System.out.println(res); // 0
+    }
+
+    public int numDecodings2(String s) {
+        int[] dp = new int[s.length() + 1];
+        dp[s.length()] = 1;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                dp[i] = 0;
+            } else {
+                dp[i] = dp[i + 1];
+                if (i + 1 < s.length()
+                        && (s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7')) {
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+
+        return dp[0];
     }
 
     public int numDecodings(String s) {
