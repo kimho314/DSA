f8e6f9758f4c16acf4a3701d28a0d0298f1b9a78
Hoseop.Kim <kimho314@gmail.com>
2026-01-01 21:57:34 +0200

    [feat]: modify MaximumProductSubarray

--- neetcode/MaximumProductSubarray.java
+++ neetcode/MaximumProductSubarray.java
@@ -12,6 +12,28 @@ public class MaximumProductSubarray {
     }
 
     public int maxProduct(int[] nums) {
+        if (nums.length == 0) {
+            return Integer.MIN_VALUE;
+        }
+        int len = nums.length;
+        int[][] dp = new int[len][2];
+        dp[0][0] = nums[0];
+        dp[0][1] = nums[0];
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(nums[i], Math.max(dp[i - 1][0] * nums[i], dp[i - 1][1] * nums[i]));
+            dp[i][1] = Math.min(nums[i], Math.min(dp[i - 1][0] * nums[i], dp[i - 1][1] * nums[i]));
+        }
+
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < len; i++) {
+            res = Math.max(res, dp[i][0]);
+            // System.out.println(i + " " + dp[i][0] + " " + dp[i][1]);
+        }
+
+        return res;
+    }
+
+    public int maxProduct2(int[] nums) {
         int res = nums[0];
         int curMin = 1, curMax = 1;
 
