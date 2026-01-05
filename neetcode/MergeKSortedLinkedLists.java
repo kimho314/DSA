c19d83436f015bdd7c064559e87a0a12adf011bd
Hoseop.Kim <kimho314@gmail.com>
2026-01-05 11:47:13 +0200

    [feat]: modify MergeKSortedLinkedLists

--- neetcode/MergeKSortedLinkedLists.java
+++ neetcode/MergeKSortedLinkedLists.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class MergeKSortedLinkedLists {
     public static void main(String[] args) {
         MergeKSortedLinkedLists sol = new MergeKSortedLinkedLists();
-        ListNode root = sol.mergeKLists2(sol.create(new Integer[][] {{1, 2, 4}, {1, 3, 5}, {3, 6}}));
+        ListNode root =
+                sol.mergeKLists2(sol.create(new Integer[][] {{1, 2, 4}, {1, 3, 5}, {3, 6}}));
         sol.print(root);
         root = sol.mergeKLists2(sol.create(new Integer[][] {}));
         sol.print(root);
@@ -61,7 +63,51 @@ public class MergeKSortedLinkedLists {
         return res;
     }
 
+    private static class Pair implements Comparable<Pair> {
+        public int val;
+        public int listIdx;
+        public ListNode curNode;
+
+        public Pair(int val, int listIdx, ListNode node) {
+            this.val = val;
+            this.listIdx = listIdx;
+            this.curNode = node;
+        }
+
+        @Override
+        public int compareTo(Pair other) {
+            return Integer.compare(this.val, other.val);
+        }
+    }
+
     public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+
+        int len = lists.length;
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for (int i = 0; i < len; i++) {
+            pq.offer(new Pair(lists[i].val, i, lists[i]));
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (!pq.isEmpty()) {
+            Pair p = pq.poll();
+            cur.next = p.curNode;
+            cur = cur.next;
+            ListNode next = p.curNode.next;
+            if (next != null) {
+                pq.offer(new Pair(next.val, p.listIdx, next));
+            }
+
+        }
+
+        return dummy.next;
+    }
+
+    public ListNode mergeKLists3(ListNode[] lists) {
         List<Integer> nodes = new ArrayList<>();
         for (ListNode list : lists) {
             while (list != null) {
@@ -82,15 +128,15 @@ public class MergeKSortedLinkedLists {
     }
 
     public ListNode mergeKLists2(ListNode[] lists) {
-        if(lists == null || lists.length == 0){
+        if (lists == null || lists.length == 0) {
             return null;
         }
 
-        while(lists.length > 1){
+        while (lists.length > 1) {
             List<ListNode> mergedLists = new ArrayList<>();
-            for(int i=0; i<lists.length; i+=2){
+            for (int i = 0; i < lists.length; i += 2) {
                 ListNode l1 = lists[i];
-                ListNode l2 = (i+1) < lists.length ? lists[i+1] : null;
+                ListNode l2 = (i + 1) < lists.length ? lists[i + 1] : null;
 
                 mergedLists.add(merge(l1, l2));
             }
@@ -100,15 +146,15 @@ public class MergeKSortedLinkedLists {
         return lists[0];
     }
 
-    private ListNode merge(ListNode l1, ListNode l2){
+    private ListNode merge(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
         ListNode cur = dummy;
 
-        while(l1 != null && l2 != null){
-            if(l1.val <= l2.val){
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
                 cur.next = l1;
                 l1 = l1.next;
-            }else{
+            } else {
                 cur.next = l2;
                 l2 = l2.next;
             }
@@ -116,9 +162,9 @@ public class MergeKSortedLinkedLists {
             cur = cur.next;
         }
 
-        if(l1 != null){
+        if (l1 != null) {
             cur.next = l1;
-        }else{
+        } else {
             cur.next = l2;
         }
 
