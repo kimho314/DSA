281117c6566ac97f62f8d5ed116595c127565e5d
Hoseop.Kim <kimho314@gmail.com>
2026-01-31 23:09:11 +0900

    [feat]: modify BalancedBinaryTree

--- neetcode/BalancedBinaryTree.java
+++ neetcode/BalancedBinaryTree.java
@@ -1,17 +1,16 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-
 public class BalancedBinaryTree {
     public static void main(String[] args) {
         BalancedBinaryTree sol = new BalancedBinaryTree();
-        TreeNode root = sol.create(new Integer[] {1, 2, 3, null, null, 4});
+        TreeNode root = sol.create(new Integer[] { 1, 2, 3, null, null, 4 });
         System.out.println(sol.isBalanced(root));
-        root = sol.create(new Integer[] {1, 2, 3, null, null, 4, null, 5});
+        root = sol.create(new Integer[] { 1, 2, 3, null, null, 4, null, 5 });
         System.out.println(sol.isBalanced(root));
         root = sol.create(new Integer[] {});
         System.out.println(sol.isBalanced(root));
-        root = sol.create(new Integer[] {1, 2, 2, 3, null, null, 3, 4, null, null, 4});
+        root = sol.create(new Integer[] { 1, 2, 2, 3, null, null, 3, 4, null, null, 4 });
         System.out.println(sol.isBalanced(root));
     }
 
@@ -20,7 +19,8 @@ public class BalancedBinaryTree {
         TreeNode left;
         TreeNode right;
 
-        TreeNode() {}
+        TreeNode() {
+        }
 
         TreeNode(int val) {
             this.val = val;
@@ -31,6 +31,24 @@ public class BalancedBinaryTree {
             this.left = left;
             this.right = right;
         }
+    }
+
+    public boolean isBalanced2(TreeNode root) {
+        return dfs(root)[0] == 1;
+    }
+
+    private int[] dfs(TreeNode root) {
+        if (root == null) {
+            return new int[] { 1, 0 };
+        }
+
+        int[] left = dfs(root.left);
+        int[] right = dfs(root.right);
+
+        boolean balanced = (left[0] == 1 && right[0] == 1) && (Math.abs(left[1] - right[1]) <= 1);
+        int height = 1 + Math.max(left[1], right[1]);
+
+        return new int[] { balanced ? 1 : 0, height };
     }
 
     public boolean isBalanced(TreeNode root) {
