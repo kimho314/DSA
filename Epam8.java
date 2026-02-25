c8da4d3a75fb40a17b0acfe415bd019192255dcb
Hoseop.Kim <kimho314@gmail.com>
2026-02-25 12:56:53 +0900

    [feat]: add Epam8

---
+++ Epam8.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+/**
+ * Palindrome String (Ignoring Special Characters) After converting uppercase letters to lowercase
+ * and removing all non-alphanumeric characters, check whether the string is a palindrome. Input: s
+ * = "A man, a plan, a canal: Panama" Processed String: amanaplanacanalpanama Output: true
+ */
+public class Epam8 {
+    public static void main(String[] args) {
+        Epam8 sol = new Epam8();
+        boolean res = sol.solution("A man, a plan, a canal: Panama");
+        IO.println(res);
+    }
+
+    private boolean solution(String s) {
+        String replaced = s.replaceAll("[^a-zA-z0-9]", "").toLowerCase();
+        return isPalindrome(replaced);
+
+    }
+
+    private boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+        while (l < r) {
+            char cl = s.charAt(l);
+            char cr = s.charAt(r);
+            // IO.println(s + " " + l + " " + cl + " " + r + " " + cr);
+            if (cl != cr) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
