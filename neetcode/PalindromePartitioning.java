e94f18c1160c7fdee27268226b449a9079aeb681
Hoseop.Kim <kimho314@gmail.com>
2026-03-27 23:24:58 +0900

    [feat]: modify PalindromePartitioning

--- neetcode/PalindromePartitioning.java
+++ neetcode/PalindromePartitioning.java
@@ -7,56 +7,34 @@ public class PalindromePartitioning {
 
     public static void main(String[] args) {
         PalindromePartitioning sol = new PalindromePartitioning();
-        System.out.println(sol.partition2("aab"));
-        System.out.println(sol.partition2("a"));
+        System.out.println(sol.partition("aab"));
+        System.out.println(sol.partition("a"));
     }
 
-    public List<List<String>> partition2(String s) {
+    public List<List<String>> partition(String s) {
         List<List<String>> res = new ArrayList<>();
         List<String> part = new ArrayList<>();
-
-        dfs2(0, s, part, res);
+        dfs(0, s, part, res);
 
         return res;
     }
 
-    private void dfs2(int i, String s, List<String> part, List<List<String>> res) {
+    private void dfs(int i, String s, List<String> part, List<List<String>> res) {
         if (i >= s.length()) {
             res.add(new ArrayList<>(part));
             return;
         }
 
         for (int j = i; j < s.length(); j++) {
-            if (isPali(s, i, j)) {
+            if (isPalindrome(s, i, j)) {
                 part.add(s.substring(i, j + 1));
-                dfs2(j + 1, s, part, res);
+                dfs(j + 1, s, part, res);
                 part.remove(part.size() - 1);
             }
         }
     }
 
-    public List<List<String>> partition(String s) {
-        dfs(0, 0, s);
-        return res;
-    }
-
-    private void dfs(int j, int i, String s) {
-        if (i >= s.length()) {
-            if (i == j) {
-                res.add(new ArrayList<>(part));
-            }
-            return;
-        }
-
-        if (isPali(s, j, i)) {
-            part.add(s.substring(j, i + 1));
-            dfs(i + 1, i + 1, s);
-            part.remove(part.size() - 1);
-        }
-        dfs(j, i + 1, s);
-    }
-
-    private boolean isPali(String s, int l, int r) {
+    private boolean isPalindrome(String s, int l, int r) {
         while (l < r) {
             if (s.charAt(l) != s.charAt(r)) {
                 return false;
