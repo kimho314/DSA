3c488171a7accaca5b79d8d273de47844934390a
Hoseop.Kim <kimho314@gmail.com>
2026-05-31 18:38:00 +0300

    [feat]: modify LongestRepeatingCharacterReplacement

---
+++ neetcode/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,35 @@
+package neetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestRepeatingCharacterReplacement {
+    static void main(String[] args) {
+        LongestRepeatingCharacterReplacement sol =
+                new LongestRepeatingCharacterReplacement();
+        IO.println(sol.characterReplacement("XYYX", 2)); // 4
+        IO.println(sol.characterReplacement("AAABABB", 1)); // 5
+        IO.println(sol.characterReplacement("AABABB", 1)); // 4
+        IO.println(sol.characterReplacement(
+                "BRJRRKNRBFOOKDEEGODTGMHNABMTHFNPTFRHRSEKKTFEQIKSIAJJMSDSLNSCNRNJFNFSIQDNMHDRIJIACLCJKATTFHDASGLRQSFN",
+                10)); // 15
+    }
+
+    public int characterReplacement(String s, int k) {
+        Map<Character, Integer> map = new HashMap<>();
+        int res = 0;
+        int maxf = 0;
+        int l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            map.put(s.charAt(r), map.getOrDefault(s.charAt(r), 0) + 1);
+            maxf = Math.max(maxf, map.get(s.charAt(r)));
+            while ((r - l + 1) - maxf > k) {
+                map.put(s.charAt(l), map.get(s.charAt(l)) - 1);
+                l++;
+            }
+            res = Math.max(res, (r - l + 1));
+        }
+
+        return res;
+    }
+}
