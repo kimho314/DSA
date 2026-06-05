52ccb26cfbb435d3e9a09963cbf5b1cbf9902564
Hoseop.Kim <kimho314@gmail.com>
2026-06-05 16:17:25 +0300

    [feat]: modify ThreeSum

--- neetcode/ThreeSum.java
+++ neetcode/ThreeSum.java
@@ -1,10 +1,6 @@
+package neetcode;
 
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * https://neetcode.io/problems/three-integer-sum?list=neetcode150
@@ -12,78 +8,29 @@ import java.util.Map;
 public class ThreeSum {
     public static void main(String[] args) {
         ThreeSum sol = new ThreeSum();
-        System.out.println(sol.threeSum2(new int[] {-1, 0, 1, 2, -1, -4})); // [[-1,-1,2],[-1,0,1]]
-        System.out.println(sol.threeSum2(new int[] {0, 1, 1})); // []
-        System.out.println(sol.threeSum2(new int[] {0, 0, 0})); // [[0,0,0]]
+        System.out.println(sol.threeSum(new int[]{-1, 0, 1, 2, -1, -4})); // [[-1,-1,2],[-1,0,1]]
+        System.out.println(sol.threeSum(new int[]{0, 1, 1})); // []
+        System.out.println(sol.threeSum(new int[]{0, 0, 0})); // [[0,0,0]]
 
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> res = new HashSet<>();
         Arrays.sort(nums);
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int num : nums) {
-            count.put(num, count.getOrDefault(num, 0) + 1);
-        }
-
-        List<List<Integer>> res = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
-            count.put(nums[i], count.get(nums[i]) - 1);
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
+            Set<Integer> set = new HashSet<>();
             for (int j = i + 1; j < nums.length; j++) {
-                count.put(nums[j], count.get(nums[j]) - 1);
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
-                    continue;
+                if (set.contains(0 - nums[i] - nums[j])) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(0 - nums[i] - nums[j]);
+                    // System.out.println(i+" "+j+" "+list);
+                    res.add(list);
                 }
-
-                int target = 0 - (nums[i] + nums[j]);
-                if (count.getOrDefault(target, 0) > 0) {
-                    res.add(Arrays.asList(nums[i], nums[j], target));
-                }
-            }
-
-            for (int j = i + 1; j < nums.length; j++) {
-                count.put(nums[j], count.get(nums[j]) + 1);
+                set.add(nums[j]);
             }
         }
-
-        return res;
+        return new ArrayList<>(res);
     }
-
-    public List<List<Integer>> threeSum2(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                break;
-            }
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            int l = i + 1;
-            int r = nums.length - 1;
-            while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-                if (sum > 0) {
-                    r--;
-                } else if (sum < 0) {
-                    l++;
-                } else {
-                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                    l++;
-                    r--;
-                    while (l < r && nums[l] == nums[l - 1]) {
-                        l++;
-                    }
-                }
-            }
-        }
-
-        return res;
-    }
-
 }
