fd1063cf22b9e3e459195ddd645acc7c3f36e488
Hoseop.Kim <kimho314@gmail.com>
2026-06-19 22:05:18 +0300

    [feat]: modify TaskScheduler

--- neetcode/TaskScheduler.java
+++ neetcode/TaskScheduler.java
@@ -1,44 +1,50 @@
-import java.util.*;
+package neetcode;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class TaskScheduler {
     public static void main(String[] args) {
         TaskScheduler sol = new TaskScheduler();
 
-        int res = sol.leastInterval(new char[] {'A', 'A', 'A', 'B', 'C'}, 3);
+        int res = sol.leastInterval(new char[]{'A', 'A', 'A', 'B', 'C'}, 3); // 9
         System.out.println(res);
-        res = sol.leastInterval(new char[] {'X', 'X', 'Y', 'Y'}, 2);
+        res = sol.leastInterval(new char[]{'X', 'X', 'Y', 'Y'}, 2); // 5
         System.out.println(res);
     }
 
     public int leastInterval(char[] tasks, int n) {
-        int[] count = new int[26];
+        int[] count = new int['Z' - 'A' + 1];
         for (char task : tasks) {
             count[task - 'A']++;
         }
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for (int cnt : count) {
             if (cnt > 0) {
-                maxHeap.add(cnt);
+                pq.add(cnt);
             }
         }
 
         int time = 0;
         Queue<int[]> q = new LinkedList<>();
-        while (!maxHeap.isEmpty() || !q.isEmpty()) {
+        while (!pq.isEmpty() || !q.isEmpty()) {
             time++;
 
-            if (maxHeap.isEmpty()) {
+            if (pq.isEmpty()) {
                 time = q.peek()[1];
-            } else {
-                int cnt = maxHeap.poll() - 1;
+            }
+            else {
+                int cnt = pq.poll() - 1;
                 if (cnt > 0) {
-                    q.add(new int[] {cnt, time + n});
+                    q.add(new int[]{cnt, time + n});
                 }
             }
 
             if (!q.isEmpty() && q.peek()[1] == time) {
-                maxHeap.add(q.poll()[0]);
+                pq.add(q.poll()[0]);
             }
         }
 
