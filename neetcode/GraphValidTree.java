67689b1f65fa73b2ed89050419fc486fa5f24f1a
Hoseop.Kim <kimho314@gmail.com>
2026-06-29 12:12:29 +0300

    [feat]: modify GraphValidTree

--- neetcode/GraphValidTree.java
+++ neetcode/GraphValidTree.java
@@ -1,11 +1,16 @@
-import java.util.*;
+package neetcode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class GraphValidTree {
     public static void main(String[] args) {
         GraphValidTree sol = new GraphValidTree();
-        boolean res = sol.validTree(5, new int[][] {{0, 1}, {0, 2}, {0, 3}, {1, 4}});
+        boolean res = sol.validTree(5, new int[][]{{0, 1}, {0, 2}, {0, 3}, {1, 4}});
         IO.println(res);
-        res = sol.validTree(5, new int[][] {{0, 1}, {1, 2}, {2, 3}, {1, 3}, {1, 4}});
+        res = sol.validTree(5, new int[][]{{0, 1}, {1, 2}, {2, 3}, {1, 3}, {1, 4}});
         IO.println(res);
     }
 
@@ -18,7 +23,6 @@ public class GraphValidTree {
         for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
-
         for (int[] edge : edges) {
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
@@ -28,7 +32,6 @@ public class GraphValidTree {
         if (!dfs(0, -1, visit, adj)) {
             return false;
         }
-
         return visit.size() == n;
     }
 
@@ -36,16 +39,18 @@ public class GraphValidTree {
         if (visit.contains(node)) {
             return false;
         }
-
         visit.add(node);
-        for (int n : adj.get(node)) {
-            if (n == parent) {
+
+        for (int elem : adj.get(node)) {
+            if (elem == parent) {
                 continue;
             }
-            if (!dfs(n, node, visit, adj)) {
+            if (!dfs(elem, node, visit, adj)) {
                 return false;
             }
         }
         return true;
     }
+
+
 }
