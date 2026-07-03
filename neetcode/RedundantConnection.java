5d8160180853bb181ba809a2629b577df5c2037b
Hoseop.Kim <kimho314@gmail.com>
2026-07-03 11:08:20 +0300

    [feat]: modify RedundantConnection

--- neetcode/RedundantConnection.java
+++ neetcode/RedundantConnection.java
@@ -7,9 +7,11 @@ import java.util.List;
 public class RedundantConnection {
     public static void main(String[] args) {
         RedundantConnection sol = new RedundantConnection();
-        int[] res = sol.findRedundantConnection(new int[][]{{1, 2}, {1, 3}, {3, 4}, {2, 4}});
+        int[] res = sol.findRedundantConnection(new int[][]{{1, 2}, {1, 3}, {3, 4}, {2, 4}}); // [2,4]
         IO.println(Arrays.toString(res));
-        res = sol.findRedundantConnection(new int[][]{{1, 2}, {1, 3}, {1, 4}, {3, 4}, {4, 5}});
+        res = sol.findRedundantConnection(new int[][]{{1, 2}, {1, 3}, {1, 4}, {3, 4}, {4, 5}}); // [3,4]
+        IO.println(Arrays.toString(res));
+        res = sol.findRedundantConnection(new int[][]{{2, 7}, {7, 8}, {3, 6}, {2, 5}, {6, 8}, {4, 8}, {2, 8}, {1, 8}, {7, 10}, {3, 9}}); // [2,8]
         IO.println(Arrays.toString(res));
     }
 
