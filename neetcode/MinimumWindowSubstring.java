c88aae72ddd7237d3039d81e75b1f328d551a58c
Hoseop.Kim <kimho314@gmail.com>
2026-09-17 14:37:13 +0200

    [feat]: modify MinimumWindowSubstring

--- neetcode/MinimumWindowSubstring.java
+++ neetcode/MinimumWindowSubstring.java
@@ -1,6 +1,7 @@
+package neetcode;
 
-
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MinimumWindowSubstring {
     public static void main(String[] args) {
@@ -11,86 +12,50 @@ public class MinimumWindowSubstring {
     }
 
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) {
+        if (s.isEmpty() || s.length() < t.length()) {
             return "";
         }
 
-        HashMap<Character, Integer> countT = new HashMap<>();
-        for (char elem : t.toCharArray()) {
-            countT.put(elem, countT.getOrDefault(elem, 0) + 1);
-        }
-
-        String res = "";
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i; j < s.length(); j++) {
-                String sub = s.substring(i, j + 1);
-                HashMap<Character, Integer> countS = new HashMap<>();
-                for (char elem : sub.toCharArray()) {
-                    countS.put(elem, countS.getOrDefault(elem, 0) + 1);
-                }
-
-                boolean isIncluded = true;
-                for (char elem : t.toCharArray()) {
-                    int cnt1 = countT.get(elem);
-                    int cnt2 = countS.getOrDefault(elem, 0);
-
-                    if (cnt1 > cnt2) {
-                        isIncluded = false;
-                        break;
-                    }
-                }
-
-                if (isIncluded) {
-                    if (res.equals("") || (!res.equals("") && res.length() > sub.length())) {
-                        res = sub;
-                    }
-                }
-            }
-
-        }
-
-        return res;
-    }
-
-    public String minWindow2(String s, String t) {
-        if (t.isEmpty())
-            return "";
-
-        Map<Character, Integer> countT = new HashMap<>();
-        Map<Character, Integer> window = new HashMap<>();
+        Map<Character, Integer> mapS = new HashMap<>();
+        Map<Character, Integer> mapT = new HashMap<>();
         for (char c : t.toCharArray()) {
-            countT.put(c, countT.getOrDefault(c, 0) + 1);
+            mapT.put(c, mapT.getOrDefault(c, 0) + 1);
         }
 
-        int have = 0, need = countT.size();
-        int[] res = {-1, -1};
-        int resLen = Integer.MAX_VALUE;
         int l = 0;
-
+        int have = 0;
+        int need = mapT.size();
+        int resLen = Integer.MAX_VALUE;
+        int[] res = {-1, -1};
         for (int r = 0; r < s.length(); r++) {
-            char c = s.charAt(r);
-            window.put(c, window.getOrDefault(c, 0) + 1);
+            char rightChar = s.charAt(r);
+            mapS.put(rightChar, mapS.getOrDefault(rightChar, 0) + 1);
 
-            if (countT.containsKey(c) && window.get(c).equals(countT.get(c))) {
+            if (mapT.containsKey(rightChar) && mapS.get(rightChar).equals(mapT.get(rightChar))) {
                 have++;
             }
 
-            while (have == need) {
+            while (need == have) {
                 if ((r - l + 1) < resLen) {
-                    resLen = r - l + 1;
+                    resLen = (r - l + 1);
                     res[0] = l;
                     res[1] = r;
                 }
 
                 char leftChar = s.charAt(l);
-                window.put(leftChar, window.get(leftChar) - 1);
-                if (countT.containsKey(leftChar) && window.get(leftChar) < countT.get(leftChar)) {
+                mapS.put(leftChar, mapS.get(leftChar) - 1);
+                if (mapT.containsKey(leftChar) && mapS.get(leftChar) < mapT.get(leftChar)) {
                     have--;
                 }
                 l++;
             }
         }
 
-        return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1] + 1);
+        if (resLen == Integer.MAX_VALUE) {
+            return "";
+        }
+        else {
+            return s.substring(res[0], res[1] + 1);
+        }
     }
 }
